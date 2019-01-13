6fb71851687f4be85c7c9b005e4eb281869b9476
ccai1 <ccai1@stuy.edu>
2019-01-13 18:35:43 -0500

    basic routing'

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.secret_key = os.urandom(32)
 #    key=data['map_key']
 #    secondkey=data['weather_key']
 
-#key = 
+#key =
 
 #determines if user is logged in or not
 
@@ -33,11 +33,11 @@ def index():
        where the users will be prompted to enter their username and password.
        When users enter and submit the information,
        they will be redirected to the authentication page.'''
-    
+
     #if the user is logged in redirect them to their profile page
     if 'user' in session:
         return redirect('/home')
-    
+
     #if not, load the login page
     return render_template("index.html")
 
@@ -62,7 +62,7 @@ def register():
     #redirects if not logged in
     if 'user' in session:
         return redirect('/home')
-    
+
     return render_template("register.html")
 
 @app.route("/auth", methods = ['POST','GET'])
@@ -74,24 +74,24 @@ def authenticate():
        If they aren't, it will redirect them to the login page and flash a message indicating the issue.
        Whether the username or the password was incorrect.
        If they were both incorrect, the message will only indicate an issue with the username.'''
-    
+
     loginStatus = ''
-    
+
     #if the user got here without entering a form, redirect them to the index
     if request.method == 'GET' or not('user' in request.form.keys()):
         return redirect('/')
-    
+
     #checks the user's login info or account creation
     if "pass2" in request.form.keys():
         loginStatus =  auth.register(request.form['user'],request.form['pass1'],request.form['pass2'])
-    
+
     else: loginStatus = auth.login(request.form["user"],request.form["pass"])
-    
+
     #if the user successsfully logs in or creates an acount, redirect them to their profile page
     if loginStatus in ["Account creation successful","Login Successful"]:
         session['user'] = request.form['user']
         return redirect('/home')
-    
+
     else:
         flash(loginStatus)
         #Redirects to previous page or root if there is none
@@ -101,7 +101,19 @@ def authenticate():
 def home():
     return render_template("home.html")
 
-    
+@app.route('/leaders')
+def leaders():
+    return render_template("leaders.html")
+
+@app.route('/market')
+def market():
+    return render_template("market.html")
+
+@app.route('/start')
+def start():
+    return render_template("start.html")
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
