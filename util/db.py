b3fb7c561e5638994b4cb8144ee9ce36a7728b10
unknown <rlin6@stuy.edu>
2019-01-14 00:18:01 -0500

    fixed images error

--- util/db.py
+++ util/db.py
@@ -8,7 +8,13 @@ c = db.cursor()
 
 #Setting up tables
 '''creates table to store user information'''
-c.execute("CREATE TABLE IF NOT EXISTS userInfo(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT, Bombermons TEXT, Upgrades TEXT, Money INTEGER)")
+c.execute("CREATE TABLE IF NOT EXISTS userInfo(userID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT, Bombermons TEXT, Upgrades TEXT, Money INTEGER)")
+
+'''creates table to store leaderboard information'''
+c.execute("CREATE TABLE IF NOT EXISTS leaderboard(userID INTEGER, Wins INTEGER)")
+
+'''creates table to store Pokemon information'''
+c.execute("CREATE TABLE IF NOT EXISTS userInfo(pokeID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, Attack INTEGER, Speed INTEGER, Type TEXT)")
 
 db.commit()
 db.close()
