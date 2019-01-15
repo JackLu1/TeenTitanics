9d17318d1f35b74c1719607ed968b308a80919f0
unknown <rlin6@stuy.edu>
2019-01-14 19:26:16 -0500

    pokeapi beginning

--- static/scripts.js
+++ static/scripts.js
@@ -10,16 +10,16 @@ var xCor=50;
 var yCor=50;
 //image processing
 var wall = new Image();
-wall.src="../static/wall.png";
+wall.src="wall.png";
 var wallV=1;
 var brick = new Image();
-brick.src="../static/brick.png";
+brick.src="brick.png";
 var brickV=2;
 var bomb = new Image();
-bomb.src="../static/bomb.png";
+bomb.src="bomb.png";
 var bombV=3;
 var explosion = new Image();
-explosion.src="../static/explosion4.png";
+explosion.src="explosion4.png";
 var explosionV=4;
 var empty=0;
 function Pokemon(name,speed){
@@ -27,7 +27,7 @@ function Pokemon(name,speed){
 	this.hp=50;
 	this.speed=speed;
 	this.img = new Image();
-	this.img.src="../static/"+name+".png";
+	this.img.src= name+".png";
 	this.maxBomb=2;
 }
 var tileX=(xCor/50)|0;
--- util/db.py
+++ util/db.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, urllib.request, json
 
 #open("../data/info.db","w").close() #Resets Database
 
@@ -14,7 +14,26 @@ c.execute("CREATE TABLE IF NOT EXISTS userInfo(userID INTEGER PRIMARY KEY AUTOIN
 c.execute("CREATE TABLE IF NOT EXISTS leaderboard(userID INTEGER, Wins INTEGER)")
 
 '''creates table to store Pokemon information'''
-c.execute("CREATE TABLE IF NOT EXISTS userInfo(pokeID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, Attack INTEGER, Speed INTEGER, Type TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS pokeInfo(pokeID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, Health INTEGER, Attack INTEGER, Speed INTEGER, Type TEXT)")
+
+#for i in 
+poke = "https://pokeapi.co/api/v2/pokemon/squirtle/"
+response = urllib.request.Request(poke)
+response.add_header('User-Agent',"Mozilla")
+response = urllib.request.urlopen(response)
+obj = json.loads(response.read())
+
+name = obj['name']
+health = obj['stats'][5]['base_stat']
+attack = obj['stats'][4]['base_stat']
+speed = obj['stats'][0]['base_stat']
+typ = obj['types'][0]['type']['name']
+
+print(name)
+print(health)
+print(attack)
+print(speed)
+print(typ)
 
 db.commit()
 db.close()
